b852a7d0a71ccc2c658b3c289b8059ca37b134ea
vaibhavbuccha <jainvaibhav415@gmail.com>
2017-10-24 14:44:03 +0530

    updatemainactivity.java

--- app/src/main/java/com/akshaybengani/currencyconverter/MainActivity.java
+++ app/src/main/java/com/akshaybengani/currencyconverter/MainActivity.java
@@ -7,20 +7,20 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+// it only change thye doller in indian rupees
     public void convert(View view)
     {
         EditText valueDoller;
         double amountRs,amountDoller;
         valueDoller=(EditText) findViewById(R.id.doller);
         amountDoller=Double.parseDouble(valueDoller.getText().toString());
-        amountRs=amountDoller*64;
+        amountRs=amountDoller*67;
         Toast.makeText(MainActivity.this,""+amountRs, Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInst1anceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
